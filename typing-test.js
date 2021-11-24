5cb2b8af36dac70c91fe170befa5bfbdb7b2a525
Pedram <pedramnili@gmail.com>
2021-11-24 07:40:55 +0330

    change

--- typing-test.js
+++ typing-test.js
@@ -23,8 +23,15 @@ initializeTest({timeLimit: TIME_LIMIT, text: TEXT});
 
 textArea.addEventListener("input", update);
 
+function initializeTest({timeLimit, text}) {
+    for (x in text) {
+        let span = document.createElement("span")
+        MyText = document.createTextNode(text[x]);
+        span.appendChild(MyText);
+        typeText.appendChild(span);
+    }
+    timerText.innerHTML = TIME_LIMIT;
 }
-  
 
 function update() {
     if (!hasStarted) {
@@ -52,13 +59,15 @@ function updateErrors() {
 }
 
 function updateWpm() {
-
+    const wpm = ((typedCharacter / 5) / timeElapsed) * 60;
+    const round = Math.round(wpm);
+    return round;
 }
 
 function updateTimer() {
-    // TODO: Complete this function
+    timerText.value === '0' ? (clearTimeout(thisFunction)) && (textArea.disabled = true) : false;
 }
 
 function finishTest() {
-  timerText.value === '0' ? (clearTimeout(thisFunction)) && (textArea.disabled = true) : false;
+    // TODO: Complete this function
 }
